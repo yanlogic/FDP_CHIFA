36aa8500d90209f133e6de0fc0ec71b5a15d6e99
Fa <Falinhe62@gmail.com>
2026-07-01 08:49:15 -0500

    Subiendo la clase gestor

---
+++ FDP_CHIFA-ChifaLogica_Jeremy/Evaluacion_Final/Chifa.App/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chifa.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Aquí irá el programa principal
+            // -- Configuración visual del sistema
+            Console.Title = "Sistema Chifa Fa v2.0";
+            Console.CursorVisible = false;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            try { Console.SetWindowSize(90, 42); } catch { }
+        }
+    }
+}
---
+++ FDP_CHIFA-ChifaLogica_Jeremy/Evaluacion_Final/Chifa.Entidades/Plato.cs
@@ -0,0 +1,18 @@
+﻿namespace Chifa.Entidades
+{
+    public class Plato
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+        public string Categoria { get; set; }
+
+        public Plato(string codigo, string nombre, double precio, string categoria)
+        {
+            Codigo = codigo;
+            Nombre = nombre;
+            Precio = precio;
+            Categoria = categoria;
+        }
+    }
+}
